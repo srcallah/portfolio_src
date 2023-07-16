2b37645771225d5e649ad8c33a9352348cb9a791
srcallah <44887032+srcallah@users.noreply.github.com>
2023-07-16 18:23:10 -0400

    debug

--- portfolio_src/js/plugins.js
+++ portfolio_src/js/plugins.js
@@ -21,6 +21,17 @@
   }
 }());
 
+import EmblaCarousel from 'embla-carousel'
+
+const emblaNode = document.querySelector('.embla')
+const options = { loop: false }
+
+const emblaApi = EmblaCarousel(emblaNode, options)
+
+console.log(emblaApi.slideNodes()) // Access API
+
+
+
 
 
 
