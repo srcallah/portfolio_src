552fbcc8c263c5c9a5b789ecc261c8a568e764a1
srcallah <44887032+srcallah@users.noreply.github.com>
2023-07-27 12:16:03 -0400

    Redesign
    click to view next slide

--- portfolio_src/js/plugins.js
+++ portfolio_src/js/plugins.js
@@ -1,33 +1,69 @@
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-  ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
 
-  while (length--) {
-    method = methods[length];
-
-    // Only stub undefined methods.
-    if (!console[method]) {
-      console[method] = noop;
-    }
+const srcPort = new Swiper('.swiper-container', {
+  lazy: true,
+  effect: 'fade',
+  loop: 'true',
+  direction: 'vertical',
+          slidesPerView: 1,
+          spaceBetween: 30,
+          mousewheel: true,
+          pagination: {
+              el: '.swiper-pagination',
+              type: 'fraction',
+              clickable: true,
+              renderFraction: function (currentClass, totalClass) {
+                  return '<span class="' + currentClass + '"></span>' +
+                          ' / ' +
+                          '<span class="' + totalClass + '"></span>';
+              },
+          },
+          
+          on: {
+  init: function () {
+      const active = document.querySelector('.swiper-slide-visible');
+      const static = document.querySelector('.invt, .designer-info');
+      const mix = active.classList.contains('mix');
+      
+      if (mix == true) {
+          static.classList.add('blend-mode');
+      } else {
+          static.classList.remove('blend-mode');
+      }
+          
+  },
+  },
+      });
+  
+  srcPort.on('slideChange', function () {
+      const active = document.querySelector('.swiper-slide-visible');
+      const static = document.querySelector('.invt, .designer-info');
+      const mix = active.classList.contains('mix');
+  
+      if (mix == true) {
+          static.classList.add('blend-mode');
+      } else {
+          static.classList.remove('blend-mode');
+      }
+  });
+  
+      divElem = document.querySelector(".designer-info");
+      elemHgt = divElem.offsetHeight;
+      var divs = document.querySelectorAll('.fig-frame');
+      for (var i = 0; i < divs.length; i++) {
+      divs[i].style.height = 'calc(100% - ' + elemHgt + 'px)';
+      }
+  
+  function resize() {
+     divElem = document.querySelector(".designer-info");
+     elemHgt = divElem.offsetHeight;
+      var divs = document.querySelectorAll('.fig-frame');
+      for (var i = 0; i < divs.length; i++) {
+      divs[i].style.height = 'calc(100% - ' + elemHgt + 'px)';
+      }
   }
-}());
-
-
-
-
-
-
-
-
-
-
-
+  window.onresize = resize;
+  
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  
+  srcPort.on('click',function(swiper,event){ swiper.slideNext(); }); 
